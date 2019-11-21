29e03f5c6eb33808b167e432ee852386dec0fbe0
Jonathan Le <jle041@sledge.cs.ucr.edu>
2019-11-20 22:23:22 -0800

    updated sql files

--- Lab07/create_indexes.sql
+++ Lab07/create_indexes.sql
@@ -1,1 +1,49 @@
---
+CREATE INDEX supplier_id_index
+ON supplier
+[USING BTREE]
+(supplier_id);
+
+CREATE INDEX supplier_name_index
+ON supplier
+[USING BTREE]
+(supplier_name);
+
+CREATE INDEX part_nyc_part_number_index
+ON part_nyc
+[USING BTREE]
+(part_number);
+
+CREATE INDEX part_nyc_supplier_index
+ON part_nyc
+[USING BTREE]
+(supplier);
+
+CREATE INDEX part_nyc_color_index
+ON part_nyc
+[USING BTREE]
+(color);
+
+CREATE INDEX part_nyc_on_hand_index
+ON part_nyc
+[USING BTREE]
+(on_hand);
+
+CREATE INDEX part_sfo_part_number_index
+ON part_sfo
+[USING BTREE]
+(part_number);
+
+CREATE INDEX part_sfo_supplier_index
+ON part_sfo
+[USING BTREE]
+(supplier);
+
+CREATE INDEX part_sfo_color_index
+ON part_sfo
+[USING BTREE]
+(color);
+
+CREATE INDEX part_sfo_on_hand_index
+ON part_sfo
+[USING BTREE]
+(on_hand);
--- Lab07/queries.sql
+++ Lab07/queries.sql
@@ -1,1 +1,51 @@
---
+--Count how many parts in NYC have more than 70 parts on_hand
+SELECT COUNT(*)
+FROM part_nyc NYC
+WHERE on_hand > 70;
+
+--Count how many total parts on_hand, in both NYC and SFO, are Red
+SELECT SUM(
+(SELECT COUNT(NYC.on_hand)
+FROM part_nyc NYC, color C
+WHERE NYC.color = C.color_id AND C.color_name = 'Red')
++
+(SELECT COUNT(SFO.on_hand)
+FROM part_sfo SFO, color C
+WHERE SFO.color = C.color_id AND C.color_name = 'Red'))
+AS sum_red;
+
+--List all the suppliers that have more total on_hand parts in NYC than they do in SFO
+SELECT S.supplier_id, S.supplier_name
+FROM supplier S
+WHERE
+(SELECT SUM(NYC.on_hand)
+FROM part_nyc NYC
+WHERE S.supplier_id = NYC.supplier)
+>
+(SELECT SUM(SFO.on_hand)
+FROM part_sfo SFO
+WHERE S.supplier_id = SFO.supplier)
+ORDER BY S.supplier_id;
+
+--List all suppliers that supply parts in NYC that aren't supplied by anyone in SFO
+SELECT DISTINCT S.supplier_id, S.supplier_name
+FROM supplier S, part_nyc NYC
+WHERE S.supplier_id = NYC.supplier AND NYC.part_number IN
+	(SELECT NYC1.part_number
+	FROM supplier S, part_nyc NYC1
+	WHERE S.supplier_id = NYC1.supplier
+	EXCEPT
+	SELECT SFO.part_number
+	FROM supplier S, part_sfo SFO
+	WHERE S.supplier_id = SFO.supplier)
+ORDER BY S.supplier_id;
+
+
+--Update all of the NYC on_hand values to on_hand - 10
+UPDATE part_nyc
+SET on_hand = on_hand - 10;
+
+--Delete all parts from NYC which have less than 30 parts on_hand
+DELETE FROM part_nyc
+WHERE on_hand < 30;
+
